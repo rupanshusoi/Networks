bb324ab07cda697e54813a012692bfc9b039e393
rupanshusoi <rupanshusoi@gmail.com>
2021-02-27 02:19:03 +0530

    Remove headers and download the reqd image

--- http_proxy_download.c
+++ http_proxy_download.c
@@ -3,11 +3,14 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netdb.h>
 #include <assert.h>
  
-#define MAX_LEN_MSG 500
-#define RECV_BUF 4096
+#define MAX_LEN_MSG 512
+#define RECV_BUF 8192
+#define BASE64_FACTOR 4
+#define TIMEOUT 5
 
 int get_socket(char *host, char *port) {
     struct addrinfo hints, *res;
@@ -26,27 +29,33 @@ int get_socket(char *host, char *port) {
 
     freeaddrinfo(res);
 
+    struct timeval tv;
+    tv.tv_sec = TIMEOUT;
+    tv.tv_usec = 0;
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof(tv));
+
     return sock;
 }
 
-char *base64_encode(char *input_str, int len_str) 
+char *base64_encode(char *input_str, int len) 
 { 
+  // TODO: Obfuscate
   char *char_set = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"; 
-  char *result = malloc(100 * sizeof(char)); 
+  char *result = malloc(BASE64_FACTOR * len); 
   int idx, num_bits = 0, padding = 0, value = 0, count = 0, tmp; 
 
   int i, j, k = 0; 
-  for (i = 0; i < len_str; i += 3) { 
+  for (i = 0; i < len; i += 3) { 
     value = 0, count = 0, num_bits = 0; 
 
-    for (j = i; j < len_str && j <= i + 2; j++) { 
+    for (j = i; j < len && j <= i + 2; j++) { 
       value = value << 8; 
       value = value | input_str[j]; 
       count++; 
     } 
     
     num_bits = count * 8; 
-    padding = num_bits % 3; 
+    padding = num_bits % 3;
     while (num_bits != 0) { 
       if (num_bits >= 6) {
         tmp = num_bits - 6; 
@@ -71,50 +80,87 @@ char *base64_encode(char *input_str, int len_str)
 } 
 
 char *encode(char *username, char *password) {
-  // TODO: Obfuscate
   int len = strlen(username) + strlen(password) + 1;
   char *data = calloc(len, sizeof(char));
   sprintf(data, "%s:%s", username, password);
   return base64_encode(data, len);
 }
 
-void make_msg(char* msg, char *URL, char *username, char *password) {
+void make_msg(char* msg, char *URL, char *server, char *username, char *password) {
   sprintf(msg, "GET http://%s HTTP/1.1\r\nHost: "
-               "http://%s\r\nProxy-Authorization: Basic ", URL, URL);
+               "http://%s\r\nProxy-Authorization: Basic ", URL, server);
   strcat(msg, encode(username, password));
   strcat(msg, "\r\n\r\n");
 }
 
 int main(int argc, char **argv)
 {
-    char *URL = argv[1];
-    char *host = argv[2];
-    char *port = argv[3];
-    char *username = argv[4];
-    char *password = argv[5];
-    char *html_filename = argv[6];
-    char *image_file = argv[7];
+    char *URL            = argv[1];
+    char *host           = argv[2];
+    char *port           = argv[3];
+    char *username       = argv[4];
+    char *password       = argv[5];
+    char *html_filename  = argv[6];
+    char *image_filename = argv[7];
 
     char *msg = calloc(MAX_LEN_MSG, sizeof(char));
-    make_msg(msg, URL, username, password);
+    make_msg(msg, URL, URL, username, password);
 
     int sock = get_socket(host, port);
+
     send(sock, msg, strlen(msg), 0);
  
     FILE *html_file;
+    html_file = fopen(html_filename, "w");
+    assert(html_file);
 
-    int bytes_read;
+    int bytes_read = 0;
+    char* header_ptr;
     char buffer[RECV_BUF];
-    do {
-        bytes_read = recv(sock, buffer, RECV_BUF, 0);
-        assert(bytes_read != -1);
+    while (1) {
+      if ((bytes_read = recv(sock, buffer, RECV_BUF, 0)) == -1) {
+        break;
+      }
 
-        html_file = fopen(html_filename, "a");
-        assert(html_file);
+      // Assume that the *entire* header will be received in one go
+      header_ptr = strstr(buffer, "\r\n\r\n");
+      if (header_ptr) {
+        fprintf(html_file, "%.*s", bytes_read - (int)(header_ptr + 4 - buffer), header_ptr + 4);
+      } else {
         fprintf(html_file, "%.*s", bytes_read, buffer);
-        fclose(html_file);
-    } while (bytes_read > 0);
- 
+      }
+    }
+    fclose(html_file);
+
+    // Download image
+    if (strcmp(URL, "info.in2p3.fr") == 0) {
+      FILE *image_file;
+      image_file = fopen(image_filename, "ab");
+      assert(image_file);
+
+      char* image_URL = calloc(strlen(URL) + strlen("/cc.gif"), sizeof(char));
+      strcat(image_URL, URL);
+      strcat(image_URL, "/cc.gif");
+      make_msg(msg, image_URL, URL, username, password);
+      
+      send(sock, msg, strlen(msg), 0);
+
+      while (1) {
+        if ((bytes_read = recv(sock, buffer, RECV_BUF, 0)) == -1) {
+          break;
+        }
+
+        // Assume that the *entire* header will be received in one go
+        header_ptr = strstr(buffer, "\r\n\r\n");
+        if (header_ptr) {
+          fwrite(buffer, bytes_read - (int)(header_ptr + 4 - buffer), 1, image_file);
+        } else {
+          fwrite(buffer, bytes_read, 1, image_file);
+        }
+      }
+      fclose(image_file);
+    }
+
     close(sock);
     return 0;
 }
