960d4fde13ec479044acd41ce6f93ccc34cc01f1
Harshit-Gakhar <harshitgakhar2000@gmail.com>
2021-02-27 23:41:22 +0530

    added redirects, check TODOs

--- http_proxy_download.c
+++ http_proxy_download.c
@@ -12,6 +12,16 @@
 #define BASE64_FACTOR 4
 #define TIMEOUT 2
 
+char* getLocation(char* buffer){
+  char* ans;
+  char* locStart = strstr(buffer, "Location: ");
+  char* locEnd = strstr(locStart, "\r\n");
+  *locEnd = '\0';
+  strcpy(ans, locStart+10);
+  *locEnd = '\r';
+  return ans;
+}
+
 int get_socket(char *host, char *port) {
     struct addrinfo hints, *res;
     memset(&hints, 0, sizeof(hints));
@@ -86,15 +96,18 @@ char *encode(char *username, char *password) {
   return base64_encode(data, len);
 }
 
-void make_msg(char* msg, char *URL, char *server, char *username, char *password) {
-  sprintf(msg, "GET http://%s HTTP/1.1\r\nHost: "
-               "http://%s\r\nProxy-Authorization: Basic ", URL, server);
+
+void make_msg(char* msg, char *URL, char *host, char *username, char *password) {
+  // todo: removed http, add it if its needed
+  sprintf(msg, "GET %s HTTP/1.1\r\nHost: "
+               "%s\r\nProxy-Authorization: Basic ", URL, host);
   strcat(msg, encode(username, password));
   strcat(msg, "\r\n\r\n");
 }
 
 int main(int argc, char **argv)
 {
+    // TODO: Make Sure URL has http://
     char *URL            = argv[1];
     char *host           = argv[2];
     char *port           = argv[3];
@@ -103,59 +116,69 @@ int main(int argc, char **argv)
     char *html_filename  = argv[6];
     char *image_filename = argv[7];
 
-    char *msg = calloc(MAX_LEN_MSG, sizeof(char));
-    make_msg(msg, URL, URL, username, password);
-
     int sock = get_socket(host, port);
-
-    send(sock, msg, strlen(msg), 0);
- 
-    int bytes_read = 0, total_bytes_read = 0;
-    char* header_ptr;
     char* buffer = malloc(RECV_BUF * sizeof(char));
+    do{
+      char *msg = calloc(MAX_LEN_MSG, sizeof(char));
+      make_msg(msg, URL, URL, username, password);
 
-    while (1) {
-      if ((bytes_read = recv(sock, buffer + total_bytes_read, RECV_BUF, 0)) == -1) {
-        break;
-      }
-      total_bytes_read += bytes_read;
-    }
-
-    FILE *html_file;
-    html_file = fopen(html_filename, "w");
-    assert(html_file);
-
-    header_ptr = strstr(buffer, "\r\n\r\n");
-    assert(header_ptr);
-    fprintf(html_file, "%.*s", total_bytes_read - (int)(header_ptr + 4 - buffer), header_ptr + 4);
-    fclose(html_file);
-
-    if (strcmp(URL, "info.in2p3.fr") == 0) {
-      char* image_URL = calloc(strlen(URL) + strlen("/cc.gif"), sizeof(char));
-      strcat(image_URL, URL);
-      strcat(image_URL, "/cc.gif");
-      make_msg(msg, image_URL, URL, username, password);
-      
+      printf("msg sent: %s", msg);
       send(sock, msg, strlen(msg), 0);
-
-      total_bytes_read = 0;
+  
+      int bytes_read = 0, total_bytes_read = 0;
+      char* header_ptr;
       while (1) {
-        if ((bytes_read = recv(sock, buffer + total_bytes_read, RECV_BUF, 0)) == -1) {
+        if ((bytes_read = recv(sock, buffer + total_bytes_read, RECV_BUF, 0)) <= 0) {
           break;
         }
         total_bytes_read += bytes_read;
       }
+      *(buffer+total_bytes_read) = '\0';
 
-      FILE *image_file;
-      image_file = fopen(image_filename, "wb");
-      assert(image_file);
-
+      if(strstr(buffer, "HTTP/1.1 30") && strstr(buffer, "HTTP/1.1 30") < strstr(buffer, "\r\n\r\n")){
+        printf("Redirect Detected\n");
+        char* location;
+        location = getLocation(buffer); 
+        strcpy(URL, location);
+        continue;
+      }
+      FILE *html_file;
+      html_file = fopen(html_filename, "w");
+      assert(html_file);
+      // printf("%s", buffer);
       header_ptr = strstr(buffer, "\r\n\r\n");
       assert(header_ptr);
-      fwrite(header_ptr + 4, total_bytes_read - (int)(header_ptr + 4 - buffer), 1, image_file);
-      fclose(image_file);
-    }
+      fprintf(html_file, "%.*s", total_bytes_read - (int)(header_ptr + 4 - buffer), header_ptr + 4);
+      fclose(html_file);
 
+      if (strcmp(URL, "info.in2p3.fr") == 0) {
+        char* image_URL = calloc(strlen(URL) + strlen("/cc.gif"), sizeof(char));
+        strcat(image_URL, URL);
+        strcat(image_URL, "/cc.gif");
+        make_msg(msg, image_URL, URL, username, password);
+        
+        send(sock, msg, strlen(msg), 0);
+
+        total_bytes_read = 0;
+        while (1) {
+          if ((bytes_read = recv(sock, buffer + total_bytes_read, RECV_BUF, 0)) <= 0) {
+            break;
+          }
+          total_bytes_read += bytes_read;
+        }
+
+        FILE *image_file;
+        image_file = fopen(image_filename, "wb");
+        assert(image_file);
+
+        header_ptr = strstr(buffer, "\r\n\r\n");
+        assert(header_ptr);
+        fwrite(header_ptr + 4, total_bytes_read - (int)(header_ptr + 4 - buffer), 1, image_file);
+        fclose(image_file);
+      }
+      break;
+    }while(1);
+    printf("%s", buffer);
     close(sock);
     return 0;
 }
