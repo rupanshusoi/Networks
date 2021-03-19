433e8df020d25ba64e46655be8d8a71fe49bec2b
rupanshusoi <rupanshusoi@gmail.com>
2021-03-19 23:57:57 +0530

    Make separate file for globals

---
+++ protocol/globals.rkt
@@ -0,0 +1,12 @@
+(define ADDR "127.0.0.1")
+(define SERVER-PORT 1060)
+(define CLIENT-PORT 1061)
+(define WINDOW-SIZE 10)
+(define BUFFER-SIZE 1000)
+(define BODY-SIZE 10)
+
+(define SYN 0)
+(define DATA 1)
+(define ACK 2)
+(define FIN 3)
+
